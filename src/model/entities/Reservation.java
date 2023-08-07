6703c09512af542220c3612eda92f652fae8441b
Carlos Pereira <carlosantonio.pereidasilva@gmail.com>
2023-08-07 20:37:33 -0300

    Very bad solution

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,65 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Reservation {
+	
+	private Integer roomNumber;
+	private LocalDate checkIn;
+	private LocalDate checkOut;
+	
+	private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	public Reservation() {
+	}
+	
+	public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public LocalDate getCheckIn() {
+		return checkIn;
+	}
+
+	public LocalDate getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duration() {
+		long diff = checkIn.until(checkOut, ChronoUnit.DAYS);
+		return diff;
+	}
+
+	public void updateDates(LocalDate checkIn, LocalDate checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Room ");
+		sb.append(roomNumber);
+		sb.append(", check-in: ");
+		sb.append(checkIn.format(dtf));
+		sb.append(", check-out: ");
+		sb.append(checkOut.format(dtf));
+		sb.append(", ");
+		sb.append(duration());
+		sb.append(" nights");
+		return sb.toString();
+	}
+	
+}
